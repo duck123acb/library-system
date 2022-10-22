020c01179738917cd25efb8c7cbf986ec5f33045
Danush Ramanan <dan.ramanan@gmail.com>
2022-10-22 08:26:20 -0600

    get text from txt file

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 
 int main()
 {
-	std::cout << "aaaaa\n";
+	std::fstream booksFile;
+	booksFile.open("books.txt");
+	for (std::string line; getline(booksFile, line);)
+	{
+		std::cout << line << "\n";
+	}
 	return 0;
 }
